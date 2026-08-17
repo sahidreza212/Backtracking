f50d761562164ab4d8f0dff99108ef91fc0b1a3a
Sahid Reza <sahidreza976@gmail.com>
2026-08-17 19:23:44 +0530

    Add the isValid function

--- src/main/java/org/example/Rat_In_Maze.java
+++ src/main/java/org/example/Rat_In_Maze.java
@@ -6,6 +6,11 @@ public class Rat_In_Maze {
     static int[] dr = {1,0,0,-1};
     static int[] dc = {0,-1,1,0};
 
+    // check if the cell is valid or not
+
+    public  static boolean isValid(int r , int c , int n , int [][] maze){
+
+    }
     public static void main(String[] args) {
 
         int[][] maze = {
