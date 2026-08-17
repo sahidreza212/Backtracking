3ef2dcf583b05a3c549327b531f03b08ffe626a5
Sahid Reza <sahidreza976@gmail.com>
2026-08-17 19:22:03 +0530

    Add till now

--- src/main/java/org/example/Rat_In_Maze.java
+++ src/main/java/org/example/Rat_In_Maze.java
@@ -1,6 +1,11 @@
 package org.example;
 
 public class Rat_In_Maze {
+
+    static String dir = "DLRU";
+    static int[] dr = {1,0,0,-1};
+    static int[] dc = {0,-1,1,0};
+
     public static void main(String[] args) {
 
         int[][] maze = {
