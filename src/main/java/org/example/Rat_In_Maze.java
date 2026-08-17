9fb0217bcf91acf4e96be80ef8ffffce9100b2a7
Sahid Reza <sahidreza976@gmail.com>
2026-08-17 19:19:56 +0530

    add the array

--- src/main/java/org/example/Rat_In_Maze.java
+++ src/main/java/org/example/Rat_In_Maze.java
@@ -3,5 +3,11 @@ package org.example;
 public class Rat_In_Maze {
     public static void main(String[] args) {
 
+        int[][] maze = {
+                {1, 0, 0, 0},
+                {1, 1, 0, 1},
+                {1, 1, 0, 0},
+                {0, 1, 1, 1}
+        };
     }
 }
