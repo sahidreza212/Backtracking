fd7a3f9afa511f5422c853919ed8505c36f77a06
Sahid Reza <sahidreza976@gmail.com>
2026-08-18 07:15:26 +0530

    Add the makeCombination function

--- src/main/java/org/example/Target_Sum_Combination.java
+++ src/main/java/org/example/Target_Sum_Combination.java
@@ -1,6 +1,12 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Target_Sum_Combination {
+
+    public static void makeCombination(int []arr, int remSum, ArrayList<Integer>cur,ArrayList<ArrayList<Integer>>res,int index){
+
+    }
     public static void main(String[] args) {
 
         int arr[] = {1,2,3};
