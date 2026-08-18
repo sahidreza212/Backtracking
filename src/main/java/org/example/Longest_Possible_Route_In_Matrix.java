3642305addee252014d99b81e8b225935e56bae1
Sahid Reza <sahidreza976@gmail.com>
2026-08-18 08:28:51 +0530

    Add the main function

--- src/main/java/org/example/Longest_Possible_Route_In_Matrix.java
+++ src/main/java/org/example/Longest_Possible_Route_In_Matrix.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public class Longest_Possible_Route_In_Matrix {
+    public static void main(String[] args) {
+        int[][] mat = {
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+
+        int xs = 0, ys = 0;
+        int xd = 1, yd = 7;
+    }
 }
