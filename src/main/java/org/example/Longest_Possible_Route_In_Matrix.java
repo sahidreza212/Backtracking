1ef28784087b292dd1ef0c9349c3e64890760e71
Sahid Reza <sahidreza976@gmail.com>
2026-08-18 08:30:25 +0530

    add the dfs function

--- src/main/java/org/example/Longest_Possible_Route_In_Matrix.java
+++ src/main/java/org/example/Longest_Possible_Route_In_Matrix.java
@@ -1,6 +1,11 @@
 package org.example;
 
 public class Longest_Possible_Route_In_Matrix {
+    public static int dfs(int [][]mat, int i, int j, int x,int y){
+        int m = mat.length;
+        int n = mat[0].length;
+
+    }
     public static void main(String[] args) {
         int[][] mat = {
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
