07da6ac91bb109095bdca6118ab3667ddae6a9c7
Sahid Reza <sahidreza976@gmail.com>
2026-08-18 07:20:46 +0530

    Add the full makeCombination function '

--- src/main/java/org/example/Target_Sum_Combination.java
+++ src/main/java/org/example/Target_Sum_Combination.java
@@ -5,7 +5,24 @@ import java.util.ArrayList;
 public class Target_Sum_Combination {
 
     public static void makeCombination(int []arr, int remSum, ArrayList<Integer>cur,ArrayList<ArrayList<Integer>>res,int index){
+       // base case
+        if(remSum == 0){
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        // invalid case if remSum < 0  or index >= arr.length
+        if(remSum < 0 || index >= arr.length){
+            return;
+        }
+        // add first element to the combination
+        cur.add(arr[index]);
 
+        // recursion with the same element
+        makeCombination(arr,remSum-arr[index],cur,res,index);
+
+        // backtrack(remove the current element and make the backtrack)
+        cur.remove(cur.size()-1);
+        makeCombination(arr,remSum,cur,res,index+1);
     }
     public static void main(String[] args) {
 
